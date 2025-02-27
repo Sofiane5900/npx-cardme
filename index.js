3b985656fd9e81b98a5f53c35fee8b9cfadeba75
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-02-27 16:04:06 +0100

    feat: add ASCII-styled title

---
+++ index.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+// node shebang
+
+
+import chalk from "chalk";
+import cfonts from "cfonts";
+
+cfonts.say("npx me", {
+    font: "block",
+    align: "center",
+    colors: ["blue", "black"],
+    letterSpacing: 1,
+    lineHeight: 1,
+    space: true,
+});
