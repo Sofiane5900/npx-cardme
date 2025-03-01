9cc7b905f213d7fa58b50da52f5cc384870abb16
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-03-01 13:08:57 +0100

    feat(generator): add function to create card.js

--- src/generator.js
+++ src/generator.js
@@ -2,36 +2,40 @@ import boxen from "boxen";
 import chalk from "chalk";
 import path from "path";
 
+// retrieve the command name & current directory absolute path
 export function createPackageFolder({namePackage}) {
-    const commandName = `npx ${namePackage.toLowerCase()}`
+    const packageName = `npx ${namePackage.toLowerCase()}`
     const folderPath = path.resolve(process.cwd());
 }
-export function generateCard({name,job,github,linkedin,discord}) {
 
-    name = name ? name : "Anonymous";
-    job = job ? job : "Unknown Job";
-    github = github ? `www.github.com/${github}` : "Not provided";
-    linkedin = linkedin ? `www.linkedin.com/in/${linkedin}` : "Not provided";
-    discord = discord ? discord : "Not provided";
-    const cardContent = `
-    ${chalk.bold(name)}
-    ${chalk.green(job)}
+export function createCommandFile({answers})
+{
+    const packagePath = createPackageFolder(answers.namePackage);
+    const filePath = path.join(packagePath,"card.js");
+
+    const commandContent = `#!/usr/bin/env node
+    import chalk from "chalk"
+    import boxen from "boxen"
     
-    ${chalk.blue("📂 GitHub:")} ${chalk.white(github)}
-    ${chalk.blue("🔗 LinkedIn:")} ${chalk.white(linkedin)}
-    ${chalk.magentaBright("💬 Discord:")} ${chalk.white(discord)}
+      const card = \`
+    ${chalk.bold("${userData.name}")}
+    ${chalk.green("${userData.job}")}
+    
+    ${chalk.blue("📂 GitHub:")} ${chalk.white("https://github.com/${userData.github}")}
+    ${chalk.blue("🔗 LinkedIn:")} ${chalk.white("https://linkedin.com/in/${userData.linkedin}")}
+    ${chalk.magentaBright("💬 Discord:")} ${chalk.white("${userData.discord}")}
+    \`;
+    
+    console.log(boxen(card, { padding: 1, margin: 1, borderStyle: "round", borderColor: "cyan", align: "center" }));
     `;
 
-    return boxen(cardContent, {
-        padding: 1,
-        margin: 1,
-        borderStyle: "round",
-        borderColor: "cyan",
-        align: "center",
-    });
 
-    return cardContent;
+    // write the file "card.js" and give permissions
+    fs.writeFile(filePath,commandContent)
+    fs.chmod(filePath, "755");
+
+
+    console.log(`package.json created at ${packagePath}`);
 }
 
-export
 
