e1accdfedda37e7c776faca39c0c4d1466293af7
Sofiane5900 <iguedjetalsofiane@gmail.com>
2025-03-09 11:29:46 +0100

    feat(generator.json): adding package.json generation

--- src/generator.js
+++ src/generator.js
@@ -11,8 +11,9 @@ export function createPackageFolder(namePackage) {
 }
 
 export function createCommandFile(answers) {
-  const packageCard = createPackageFolder(answers.namePackage);
+  const packagePath = createPackageFolder(answers.namePackage);
   const filePath = path.join(packagePath, "card.js");
+  const packageJsonPath = path.join(packagePath, "package.json");
 
   const commandContent = `#!/usr/bin/env node
     import chalk from "chalk"
@@ -28,11 +29,29 @@ export function createCommandFile(answers) {
     \`;
     
     console.log(boxen(card, { padding: 1, margin: 1, borderStyle: "round", borderColor: "cyan", align: "center" }));
-    `;
+     `;
 
   // write the file "card.js" and give permissions
   fs.writeFileSync(filePath, commandContent);
   fs.chmodSync(filePath, "755");
 
-  console.log(`card.js created at ${packageCard}`);
+  //package.json template
+  const packageJson = {
+    name: answers.namePackage.toLowerCase(),
+    version: "1.0.0",
+    description: `NPX business card for ${answers.name}`,
+    main: "card.js",
+    bin: {
+      [answers.namePackage]: "card.js",
+    },
+    type: "module",
+    dependencies: {
+      chalk: "^5.0.0",
+      boxen: "^7.0.0",
+    },
+    author: answers.name,
+    license: "MIT",
+  };
+
+  fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
 }
